26c548d411b465b2a96ea644fc0b54d22dafb69c
Shrenik <shrenik.jain@research.iiit.ac.in>
2015-09-16 17:27:59 +0530

    Main Added

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <cstdio>
+#include <fstream>
 
 extern "C" FILE *yyin; 
 extern "C" int yyparse(); 
-
+std::fstream flex;
+std::fstream bison;
 int main (const int argc, const char ** argv) {
+
     if (argc < 2) {
         yyin = NULL;
         std::cerr <<"fatal error: no input files" << std::endl;
@@ -21,6 +24,8 @@ int main (const int argc, const char ** argv) {
     extern int yydebug;
     yydebug = 1;
 #endif
+    flex.open("flex_output.txt",std::fstream::out | std::fstream::trunc );
+    bison.open("bison_output.txt",std::fstream::out | std::fstream::trunc );
     do {
         yyparse();
     } while (!feof(yyin));
